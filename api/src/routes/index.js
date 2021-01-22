a61e7d60b1791b5608a77a2cde6d46453c8407af
IzabellaPcn <43002486+IzabellaPcn@users.noreply.github.com>
2021-01-22 01:28:33 -0300

    atualização das rotas de editar, cria e deletar alunos

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -7,5 +7,9 @@ import AlunosController from '../app/controllers/AlunoController';
 const routes = new Router();
 
 routes.get('/alunos', AlunosController.index);
+routes.post('/alunos', AlunosController.create);
+routes.put('/alunos', AlunosController.update);
+routes.delete('/alunos', AlunosController.delete);
+
 
 export default routes;
